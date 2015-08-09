7290761f53d36c0459cad206052884f028692ff0
suparna619 <suparnad@thoughtworks.com>
2015-08-09 23:06:25 +0530

    Problem 1 - Multiples Of 3 And 5 below 1000 - Completed

---
+++ MultiplesOf3And5/MultiplesOf3And5/MultipleOf3And5.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3And5
+{
+    public class MultipleOf3And5
+    {
+        public long SumOfTheMultiplesOf3And5BelowRange(long Range)
+        {
+            long sum = 0;
+            for (int i = 1; i < Range; i++)
+            {
+                if (i%3 == 0 || i%5==0)
+                {
+                    sum += i;
+                }
+            }
+
+            return sum;
+        }
+    }
+}
--- MultiplesOf3And5/MultiplesOf3And5/Program.cs
+++ MultiplesOf3And5/MultiplesOf3And5/Program.cs
@@ -10,6 +10,9 @@ namespace MultiplesOf3And5
     {
         static void Main(string[] args)
         {
+            var SumOfTheMultiplesOf3And5 = new MultipleOf3And5();
+            Console.WriteLine(SumOfTheMultiplesOf3And5.SumOfTheMultiplesOf3And5BelowRange(1000));
+            Console.ReadLine();
         }
     }
 }
