a324e33e898b28aa449110508dfef6b6cd519a96
suparna619 <suparnad@thoughtworks.com>
2015-08-11 14:46:29 +0530

    Problem 2 - Even Fibonacci numbers -Completed

--- MultiplesOf3And5/MultiplesOf3And5/SumOfFibonaccySeries.cs
+++ MultiplesOf3And5/MultiplesOf3And5/SumOfFibonaccySeries.cs
@@ -12,18 +12,16 @@ namespace MultiplesOf3And5
         {
             var prev = 1; 
             var curr = 2;
-            var term = 3;
             var total = 2;
-            while (term <= 4000000)
+            while (curr <= 4000000)
             {
                 var mid = prev;
                 prev = curr;
                 curr += mid; 
-                if (term%2 == 0)
+                if (curr%2 == 0)
                 {
                     total += curr;
                 }
-                term++;
             }
             return total;
         }
