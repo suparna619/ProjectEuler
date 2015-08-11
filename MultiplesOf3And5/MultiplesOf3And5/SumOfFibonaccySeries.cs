3fef3660b5f050b4d769212b5ff99b5085b13cea
suparna619 <suparnad@thoughtworks.com>
2015-08-11 11:30:40 +0530

    SumOfFibonaccySeries.cs changes added to master

---
+++ MultiplesOf3And5/MultiplesOf3And5/SumOfFibonaccySeries.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MultiplesOf3And5
+{
+    class SumOfFibonaccySeries
+    {
+        public long SumOfEvenValuedTermsInTheFibonaccySeriesLessThanFourMillion()
+        {
+            var prev = 1; 
+            var curr = 2;
+            var term = 3;
+            var total = 2;
+            while (term <= 4000000)
+            {
+                var mid = prev;
+                prev = curr;
+                curr += mid; 
+                if (term%2 == 0)
+                {
+                    total += curr;
+                }
+                term++;
+            }
+            return total;
+        }
+    }
+}
